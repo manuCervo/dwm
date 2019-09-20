ec70443919ae3f733d574cb479b9d25548876ca9
Manuel Cervini <manux999@gmail.com>
2019-09-20 11:47:07 +0200

    added stuff

--- config.h
+++ config.h
@@ -95,10 +95,6 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	/*{ MODKEY,			XK_minus,  setgaps,	   {.i = -1} },
-	{ MODKEY,			XK_plus,   setgaps,	   {.i = +1} },
-	{ MODKEY|ShiftMask,		XK_minus,  setgaps,	   {.i = 0} },
-	{ MODKEY|ShiftMask,		XK_plus,   setgaps,	   {.i = 16} },*/
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
