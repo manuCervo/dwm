b4684efa564d983d1761b967fb5c1204e8bd7c1d
Manuel Cervini <manux999@gmail.com>
2019-11-11 13:09:23 +0100

    updated tagmask

--- config.h
+++ config.h
@@ -38,6 +38,7 @@ static const Rule rules[] = {
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 	{ "com-install4j-runtime-launcher-UnixLauncher", NULL, "win0",    0,1,-1 },
+	{ "java-lang-Thread",NULL,"win0",0,1,-1},
 
 };
 
