69660476f0dc4e2bb577e2e1450205aa03453b99
Manuel Cervini <manux999@gmail.com>
2019-11-13 14:48:59 +0100

    updated files

--- config.h
+++ config.h
@@ -2,8 +2,8 @@
 #include "layouts.c"
 #include "fibonacci.c"
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 16;       /* snap pixel */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
+static const unsigned int snap      = 20;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=12" };
@@ -18,12 +18,17 @@ static const char col_pink[]	    = "#d71a6c";
 static const char col_cyan[]        = "#005577";
 static const char col_light_blue[]  = "#00e3e0";
 static const char col_red[]	    = "#ff0000";
-static const unsigned int gappx     = 0;
+static const char col_light_blue2[] = "#0069b8";
+static const char col_green[]	    = "#549400";
+static const char col_orange[]	    = "#D65700";
+
+static const unsigned int gappx     = 20;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2},
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
-	{ col_red,col_gray1,col_gray2},
+	[SchemeNorm] = { col_gray3, col_light_blue2, col_gray2},
+	[SchemeSel]  = { col_gray4, col_green,  col_cyan  },
+	 { col_red,col_orange,col_gray2},
+	 { col_gray3,col_orange,col_gray2},
 };
 
 /* tagging */
@@ -49,9 +54,9 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "T",      tile },    /* first entry is default */
+	{ "F",      NULL }, 
+	{ "T",      tile },    
 	{ "G",      grid},
-	{ "F",      NULL },    /* no layout function means floating behavior */
 	{ "M",      monocle },
 	{ "S",	    dwindle},
 	{ "C",	    centeredmaster},
@@ -70,7 +75,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-i" };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_light_blue2, "-nf", col_gray3, "-sb", col_green, "-sf", col_gray4, "-i" };
 //static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_light_blue, "-sf", col_pink, "-i" };
 
 static const char *termcmd[]  = { "termite", NULL };
@@ -90,10 +95,10 @@ static Key keys[] = {
 	{ MODKEY|ControlMask,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,	                XK_q,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[3]} },
-	{ MODKEY,			XK_g,	   setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,			XK_g,	   setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,			XK_s,	   setlayout,	   {.v = &layouts[4]} },
 	{ MODKEY,			XK_c,	   setlayout,	   {.v = &layouts[5]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
